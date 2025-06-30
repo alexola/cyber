7ff01f671c24ea3490de5bb3fe56c5b1ff22f4c1
alexo <alexola.22@gmail.com>
2025-06-30 17:59:24 +0100

    Adding the new tool to the menu and upading our launch tool

--- recon-suite/main.py
+++ recon-suite/main.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import os
 
@@ -6,12 +5,12 @@ import os
 from modules import (password_checker) 
 
 def launch_tool(tool_scripts):
-  #Launch a tool script from the modules directory
-  tool_path = os.path.join("modules", tool_scripts)
-  try:
-    subprocess.run(["python3", tool_path])
-  except Exception as e:
-    print(f"Could not run tool: {e}")
+    # Corrected path to modules inside recon-suite
+    tool_path = os.path.join("recon-suite", "modules", tool_scripts)
+    try:
+        subprocess.run(["python3", tool_path])
+    except Exception as e:
+        print(f"Could not run tool: {e}")
 
 def main():
   # Print ASCII art banner
@@ -31,7 +30,8 @@ def main():
   while True:
     print("\nAvailable tools:")
     print("1. Password Checker - Check the strength of your passwords")
-    print("2. Port Scanner - Scan for open ports on a target / stealth scan (wip)")
+    print("2. Port Scanner - Scan for open ports on a target / stealth scan")
+    print("3. Subdomain Enumerator - Enumerate subdomains for a given domain")
     print("0. Exit")
 
     #we should add a loop to keep the menu running until the user decides to exit
@@ -41,6 +41,8 @@ def main():
             launch_tool("password_checker.py")
         elif choice == '2':
             launch_tool("port_scanner.py")
+        elif choice == '3':
+            launch_tool("subdomain_enumerator.py")
         elif choice == '0':
             print("Exiting Recon Suite. Goodbye!")
             break
